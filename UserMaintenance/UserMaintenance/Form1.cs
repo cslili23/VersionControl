3eb646a7d31346dddff476ac4b38bd75f41f65b3
Lili Csendes <csendilili@gmail.com>
2020-09-23 19:48:24 +0200

    Just FullName

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -7,14 +7,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using UserMaintenance.NewFolder1;
 
 namespace UserMaintenance
 {
     public partial class Form1 : Form
     {
+        BindingList<User> users = new BindingList<User>();
+
         public Form1()
         {
             InitializeComponent();
+            label1.Text = Resource1.FullName;
+            
+            button1.Text = Resource1.FullName;
+
+            listBox1.DataSource = users;
+            listBox1.ValueMember = "ID";
+            listBox1.DisplayMember = "FullName";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var u = new User()
+            {
+                FullName = textBox1.Text,
+                
+            };
+            users.Add(u);
         }
     }
 }
