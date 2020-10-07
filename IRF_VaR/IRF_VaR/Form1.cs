c3b7e9234045ef41c2f98868fcade619751ceee4
Lili Csendes <csendilili@gmail.com>
2020-10-07 17:38:25 +0200

    7

--- IRF_VaR/IRF_VaR/Form1.cs
+++ IRF_VaR/IRF_VaR/Form1.cs
@@ -23,7 +23,8 @@ namespace IRF_VaR
             Ticks = context.Ticks.ToList();
             dataGridView1.DataSource = Ticks;
 
-            CreatePortfolio(); 
+            CreatePortfolio();
+            
         }
         private void CreatePortfolio()
         {
@@ -32,6 +33,20 @@ namespace IRF_VaR
             Portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
 
             dataGridView2.DataSource = Portfolio;
+        }
+        private decimal GetPortfolioValue(DateTime date)
+        {
+            decimal value = 0;
+            foreach (var item in Portfolio)
+            {
+                var last = (from x in Ticks
+                            where item.Index == x.Index.Trim()
+                               && date <= x.TradingDay
+                            select x)
+                            .First();
+                value += (decimal)last.Price * item.Volume;
+            }
+            return value;
         }
     }
 }
