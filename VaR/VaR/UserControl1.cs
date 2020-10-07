95cfc34dda889fbdc6663abac699a77b337f6e77
Lili Csendes <csendilili@gmail.com>
2020-10-07 17:07:39 +0200

    4

--- VaR/VaR/UserControl1.cs
+++ VaR/VaR/UserControl1.cs
@@ -10,7 +10,7 @@ using System.Windows.Forms;
 
 namespace VaR
 {
-    public partial class UserControl1: UserControl
+    public partial class UserControl1 : UserControl
     {
         public UserControl1()
         {
