30f072382cf7ddbc6fa9066e8e13b3fdeba57a57
Lili Csendes <csendilili@gmail.com>
2020-10-22 16:13:51 +0200

    4

--- Mikroszimultacio/Mikroszimultacio/Form1.cs
+++ Mikroszimultacio/Mikroszimultacio/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Mikroszimultacio.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,38 @@ namespace Mikroszimultacio
 {
     public partial class Form1 : Form
     {
+        List<Person> Population = new List<Person>();
+        List<BirthProbability> BirthProbabilities = new List<BirthProbability>();
+        List<DeathProbability> DeathProbabilities = new List<DeathProbability>();
         public Form1()
         {
             InitializeComponent();
+
+            Population = GetPopulation(@"C:\Temp\nép.csv");
+            BirthProbabilities = GetBirthProbabilities(@"C:\Temp\születés.csv");
+            DeathProbabilities = GetDeathProbabilities(@"C:\Temp\halál.csv");
+
+            public List<Person> GetPopulation(string csvpath)
+            {
+                List<Person> population = new List<Person>();
+
+                using (StreamReader sr = new StreamReader(csvpath, Encoding.Default))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        var line = sr.ReadLine().Split(';');
+                        population.Add(new Person()
+                        {
+                            BirthYear = int.Parse(line[0]),
+                            Gender = (Gender)Enum.Parse(typeof(Gender), line[1]),
+                            NbrOfChildren = int.Parse(line[2])
+                        });
+                    }
+                }
+
+                return population;
+            }
+
         }
     }
 }
