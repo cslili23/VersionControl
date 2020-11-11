6589c4441337137a27b178ea6c4f929b73e83dec
Lili Csendes <csendilili@gmail.com>
2020-11-11 12:05:27 +0100

    9

--- jatekgyar/jatekgyar/Entities/CarFactory.cs
+++ jatekgyar/jatekgyar/Entities/CarFactory.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace jatekgyar.Entities
 {
-     public class BallFactory : IToyFactory
+    class CarFactory :IToyFactory
     {
         public Toy CreateNew()
         {
