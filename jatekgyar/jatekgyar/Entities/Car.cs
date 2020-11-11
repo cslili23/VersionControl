7460aab03616be5272f3f251790b1983a79010a9
Lili Csendes <csendilili@gmail.com>
2020-11-11 11:56:35 +0100

    7

---
+++ jatekgyar/jatekgyar/Entities/Car.cs
@@ -0,0 +1,18 @@
+﻿using jatekgyar.Abstraction;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jatekgyar.Entities
+{
+    class Car : Toy
+    {
+        protected override void DrawImage(Graphics g)
+        {
+            Image imageFile = Image.FromFile("Images/car.png");
+            g.DrawImage(imageFile, new Rectangle(0, 0, Width, Height));
+        }
+    }
+}
