e4b3ac7485d2ef912185fd8564990b9602bd15df
Lili Csendes <csendilili@gmail.com>
2020-11-18 15:25:12 +0100

    9

--- Unit_Test/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ Unit_Test/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -24,5 +24,48 @@ namespace UnitTestExample.Test
 
             Assert.AreEqual(expectedResult, actualResult);
         }
+        [
+         Test,
+         TestCase("irf@uni-corvinus.hu", "Abcd1234"),
+         TestCase("irf@uni-corvinus.hu", "Abcd1234567"),
+        ]
+        public void TestRegisterHappyPath(string email, string password)
+        {
+            var accountController = new AccountController();
+
+            var actualResult = accountController.Register(email, password);
+
+            Assert.AreEqual(email, actualResult.Email);
+            Assert.AreEqual(password, actualResult.Password);
+            Assert.AreNotEqual(Guid.Empty, actualResult.ID);
+        }
+        [
+          Test,
+          TestCase("irf@uni-corvinus", "Abcd1234"),
+          TestCase("irf.uni-corvinus.hu", "Abcd1234"),
+          TestCase("irf@uni-corvinus.hu", "abcd1234"),
+          TestCase("irf@uni-corvinus.hu", "ABCD1234"),
+          TestCase("irf@uni-corvinus.hu", "abcdABCD"),
+          TestCase("irf@uni-corvinus.hu", "Ab1234"),
+        ]
+        public void TestRegisterValidateException(string email, string password)
+        {
+            // Arrange
+            var accountController = new AccountController();
+
+            // Act
+            try
+            {
+                var actualResult = accountController.Register(email, password);
+                Assert.Fail();
+            }
+            catch (Exception ex)
+            {
+                Assert.IsInstanceOf<ValidationException>(ex);
+            }
+
+            // Assert
+        }
     }
+    
 }
