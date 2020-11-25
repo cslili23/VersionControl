07f9388955edab099b793582a9f6ac5719a1b507
Lili Csendes <csendilili@gmail.com>
2020-11-25 10:41:07 +0100

    6

--- Evoluvios_algoritmus/Evoluvios_algoritmus/Form1.cs
+++ Evoluvios_algoritmus/Evoluvios_algoritmus/Form1.cs
@@ -48,6 +48,21 @@ namespace Evoluvios_algoritmus
             label1.Text = string.Format(
                 "{0}. generáció",
                 generation);
+            gc.ResetCurrentLevel();
+            foreach (var p in topPerformers)
+            {
+                var b = p.Brain.Clone();
+                if (generation % 3 == 0)
+                    gc.AddPlayer(b.ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(b);
+
+                if (generation % 3 == 0)
+                    gc.AddPlayer(b.Mutate().ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(b.Mutate());
+            }
+            gc.Start();
         }
     }
 }
