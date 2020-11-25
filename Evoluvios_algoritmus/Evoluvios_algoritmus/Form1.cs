a4b990420cfe25fdfd9a80245f8ef13f5603ecbf
Lili Csendes <csendilili@gmail.com>
2020-11-25 10:38:12 +0100

    4

--- Evoluvios_algoritmus/Evoluvios_algoritmus/Form1.cs
+++ Evoluvios_algoritmus/Evoluvios_algoritmus/Form1.cs
@@ -15,13 +15,18 @@ namespace Evoluvios_algoritmus
     {
         GameController gc = new GameController();
         GameArea ga;
+
+        int populationSize = 100;
+        int nbrOfSteps = 10;
+        int nbrOfStepsIncrement = 10;
+        int generation = 1;
         public Form1()
         {
             InitializeComponent();
 
             ga = gc.ActivateDisplay();
-            gc.AddPlayer();
-            gc.Start(true);
+            //gc.AddPlayer();
+            //gc.Start(true);
             this.Controls.Add(ga);
             
         }
